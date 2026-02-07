70e19a9948e9d0bfcb0e675f04e4b8674d5ad0e0
ilenker <jpswanepoel.42@gmail.com>
2026-02-07 16:28:21 +0200

    add instruction execution logic, add flags, move enums and string methods to dedicated files

---
+++ internal/sim/string_methods.go
@@ -0,0 +1,63 @@
+package sim
+
+import (
+	"fmt"
+	"strings"
+)
+
+func (arg Argument) String() string {
+	str := strings.Builder{}
+	switch arg.Type {
+	case ArgNone:
+		str.WriteString("none")
+
+	case ArgReg:
+		str.WriteString("Register: ")
+		str.WriteString(arg.Reg.Name.String())
+		str.WriteString(arg.Reg.Subset.String())
+
+	case ArgMem:
+		str.WriteString("Memory: ")
+		str.WriteString("Base(")
+		str.WriteString(arg.Mem.BaseReg.String())
+		str.WriteString(") Index(")
+		str.WriteString(arg.Mem.IndexReg.String())
+		str.WriteString(") Disp(")
+		str.WriteString(fmt.Sprintf("%d)", arg.Mem.Disp))
+
+	case ArgImm:
+		str.WriteString("Immediate: ")
+		str.WriteString(fmt.Sprintf("%d", arg.ImmValue))
+
+	}
+	return str.String()
+}
+
+func (cpu *CPUContext) String() string {
+	str := strings.Builder{}
+	str.WriteString(cRed(fmt.Sprintf("Ax(%5d): %#02x, %#02x\n", cpu.Registers[AX], cpu.Registers[AX]>>8, byte(cpu.Registers[AX]))))
+	str.WriteString(cBlu(fmt.Sprintf("Cx(%5d): %#02x, %#02x\n", cpu.Registers[CX], cpu.Registers[CX]>>8, byte(cpu.Registers[CX]))))
+	str.WriteString(cGrn(fmt.Sprintf("Dx(%5d): %#02x, %#02x\n", cpu.Registers[DX], cpu.Registers[DX]>>8, byte(cpu.Registers[DX]))))
+	str.WriteString(cYel(fmt.Sprintf("Bx(%5d): %#02x, %#02x\n", cpu.Registers[BX], cpu.Registers[BX]>>8, byte(cpu.Registers[BX]))))
+        
+	str.WriteString(fmt.Sprintf("SP(%5d): %#04x\n", cpu.Registers[SP], cpu.Registers[SP]))
+	str.WriteString(fmt.Sprintf("BP(%5d): %#04x\n", cpu.Registers[BP], cpu.Registers[BP]))
+	str.WriteString(fmt.Sprintf("SI(%5d): %#04x\n", cpu.Registers[SI], cpu.Registers[SI]))
+	str.WriteString(fmt.Sprintf("DI(%5d): %#04x\n", cpu.Registers[DI], cpu.Registers[DI]))
+
+	return str.String()
+}
+
+func (fs Flags) String() string {
+	str := strings.Builder{}
+	if fs&FlagOverflow  != 0 { str.WriteRune('O') }
+	if fs&FlagDirection != 0 { str.WriteRune('D') }
+	if fs&FlagInterrupt != 0 { str.WriteRune('I') }
+	if fs&FlagTrap      != 0 { str.WriteRune('T') }
+	if fs&FlagSign      != 0 { str.WriteRune('S') }
+	if fs&FlagZero      != 0 { str.WriteRune('Z') }
+	if fs&FlagAuxCarry  != 0 { str.WriteRune('A') }
+	if fs&FlagParity    != 0 { str.WriteRune('P') }
+	if fs&FlagCarry     != 0 { str.WriteRune('C') }
+	return str.String()
+}
